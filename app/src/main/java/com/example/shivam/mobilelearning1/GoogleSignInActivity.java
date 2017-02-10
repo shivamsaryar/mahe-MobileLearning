3251af4e92f421158678e48035467558c6c87542
Shivam Saryar <shivamsaryar26@gmail.com>
2017-02-10 16:58:01 +0000

    Commit Feb_10th

--- app/src/main/java/com/example/shivam/mobilelearning1/GoogleSignInActivity.java
+++ app/src/main/java/com/example/shivam/mobilelearning1/GoogleSignInActivity.java
@@ -1,13 +1,75 @@
 package com.example.shivam.mobilelearning1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class GoogleSignInActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
+public class GoogleSignInActivity extends AppCompatActivity implements View.OnClickListener {
+
+    GoogleApiClient mGoogleApiClient;
+    SignInButton signInButton;
+    Button signoutButton;
+    TextView statusTextView;
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_sign_in);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, (GoogleApiClient.OnConnectionFailedListener) this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(this);
+        signoutButton = (Button) findViewById(R.id.sign_out_button);
+        signoutButton.setOnClickListener(this);
+        statusTextView = (TextView) findViewById(R.id.status_text_view);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            case R.id.sign_out_button:
+                signOut();
+                break;
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+    }
+
+    private void signOut() {
+    }
+
 }
