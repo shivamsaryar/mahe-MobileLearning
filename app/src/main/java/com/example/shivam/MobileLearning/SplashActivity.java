85588db8023a1bf5cd847368ce52bcb1950653e7
Shivam Saryar <shivamsaryar26@gmail.com>
2017-03-02 00:12:47 +0530

    Commit _March_1st

--- app/src/main/java/com/example/shivam/MobileLearning/SplashActivity.java
+++ app/src/main/java/com/example/shivam/MobileLearning/SplashActivity.java
@@ -39,7 +39,6 @@ public class SplashActivity extends AppCompatActivity {
                     signedIn = false;
                 }
                 proceed(signedIn);
-
             }
         };
         /*
