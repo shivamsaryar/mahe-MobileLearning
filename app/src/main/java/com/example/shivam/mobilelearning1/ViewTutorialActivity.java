0185c266de5918acbdc35c3ccd3c523ee331b9d3
Shivam Saryar <shivamsaryar26@gmail.com>
2017-04-04 11:20:59 +0530

    Added enrolled courses buttons in Home Fragment

--- app/src/main/java/com/example/shivam/mobilelearning1/ViewTutorialActivity.java
+++ app/src/main/java/com/example/shivam/mobilelearning1/ViewTutorialActivity.java
@@ -9,5 +9,6 @@ public class ViewTutorialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_tutorial);
+
     }
 }
